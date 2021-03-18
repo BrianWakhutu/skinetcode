fa49b4cd35c43d1f88d24be6d3a1dcb26e02a63d
Brian Wakhutu <bwakhutu@jhpiego.org>
2021-03-18 15:49:13 +0300

    End of Section 6

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,8 +32,15 @@ namespace API
              x.UseSqlite(_configuration.GetConnectionString("DefaultConnection")));
              services.AddApplicationServices();
              services.AddSwaggerDocumentation();
-            
-        }
+             services.AddCors(opt => 
+             {
+                 opt.AddPolicy("CorsPolicy", policy => 
+                 {
+
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                 });
+             });
+          }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
