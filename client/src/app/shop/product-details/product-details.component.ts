8b127f63bd39ce00641b62520f85a76994a47255
Brian Wakhutu <bwakhutu@jhpiego.org>
2021-05-09 20:43:28 +0300

    End of Section 40

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -1,3 +1,5 @@
+import { BasketService } from './../../basket/basket.service';
+
 import { ShopService } from './../shop.service';
 import { IProduct } from 'src/app/shared/models/product';
 import { Component, OnInit } from '@angular/core';
@@ -11,15 +13,29 @@ import { BreadcrumbService } from 'xng-breadcrumb';
 })
 export class ProductDetailsComponent implements OnInit {
   product: IProduct;
+  quantity = 1;
 
   constructor(private shopService: ShopService,
               private activateRoute: ActivatedRoute,
-              private bcService: BreadcrumbService) {
+              private bcService: BreadcrumbService,
+              private basketService: BasketService) {
                 this.bcService.set('@productDetails', ' ')
               }
 
   ngOnInit(): void {
     this.loadProduct();
+  }
+
+  addItemToBasket() {
+    this.basketService.addItemToBasket(this.product, this.quantity);
+  }
+  incrementQuantity() {
+    this.quantity++;
+  }
+  decrementquantity() {
+    if(this.quantity > 1) {
+      this.quantity--;
+    }
   }
   loadProduct(){
     this.shopService.getProduct(+this.activateRoute.snapshot.paramMap.get('id')).subscribe(product => {
